94989b1a0081ca27f16549dae7c7dbd608f8436a
Ignacio Errico <pedro.errico@ey.com>
2022-09-03 17:29:59 -0300

    Create a Dual Blazor Server/Blazor WebAssembly app
    - Create an app that can run as either a Blazor Server app or a Blazor WebAssembly app
      - Move all pages and components to a Razor class library (RCL)
      - Reference static assets in the RCL using "/_content/{RCL-assembly-name}"
    - References:
    [Dual Blazor Server/Blazor WebAssembly app](https://docs.microsoft.com/en-us/aspnet/core/blazor/project-structure?view=aspnetcore-6.0#dual-blazor-serverblazor-webassembly-app)
    [Consume ASP.NET Core Razor components from a Razor class library (RCL)](https://docs.microsoft.com/en-us/aspnet/core/blazor/components/class-libraries?view=aspnetcore-6.0&tabs=visual-studio)

--- src/1-Blazor/ProgrammersWeek.TalkManager.BlazorWebAssemblyApp/Program.cs
+++ src/1-Blazor/ProgrammersWeek.TalkManager.BlazorWebAssemblyApp/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ProgrammersWeek.TalkManager.BlazorWebAssemblyApp;
+using ProgrammersWeek.TalkManager.BlazorUi;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
